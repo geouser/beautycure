8f4edcc8088e0b6cda0969ca68a51be87688b2be
Serhiy <serhiyhulyi93@gmail.com>
2016-03-02 19:47:38 +0200

    photogallery

--- js/main.js
+++ js/main.js
@@ -54,6 +54,37 @@ jQuery(document).ready(function($) {
     }
   });
 
+
+
+
+
+  $('.slider').on('init', function(event) {
+    event.preventDefault();
+    $('.slider').magnificPopup({
+      delegate: 'figure',
+      type: 'image',
+      fixedContentPos: false,
+      fixedBgPos: true,
+      overflowY: 'auto',
+      modal: false,
+      closeBtnInside: true,
+      preloader: false,
+      midClick: true,
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      }
+    });
+  });
+  
+  $('.slider').slick({
+    infinite: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+  })
+  
+
 });
 
 
